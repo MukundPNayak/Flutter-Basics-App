1d8cfaef58306c32a281d770c2711a72c989133b
Mukund Nayak <mukundpnayak2000@gmail.com>
2020-10-24 02:31:21 +0530

    added main dart file

---
+++ main.dart
@@ -0,0 +1,45 @@
+import 'package:Shops_app/screens/orders_screen.dart';
+
+import './screens/product_details_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './screens/product_overview_screen.dart';
+import './screens/user_product_screen.dart';
+import './screens/edit_products_screen.dart';
+import './screens/cart_screen.dart';
+import './providers/products.dart';
+import './providers/cart.dart';
+import './providers/orders.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Products(),
+        ),
+        ChangeNotifierProvider(create: (ctx) => Cart()),
+        ChangeNotifierProvider(create: (ctx) => Orders()),
+      ],
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.orange,
+          fontFamily: 'Lato',
+        ),
+        home: ProductOverviewScreen(),
+        routes: {
+          ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen(),
+          CartScreen.routeName: (ctx) => CartScreen(),
+          OrdersScreen.routeName: (ctx) => OrdersScreen(),
+          UserProductScreen.routeName: (ctx) => UserProductScreen(),
+          EditProductScreen.routeName: (ctx) => EditProductScreen(),
+        },
+      ),
+    );
+  }
+}
